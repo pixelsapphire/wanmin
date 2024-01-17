01024766f4505ad65d8da41545961e9c6afb7057
pixelsapphire <rubynaxela@gmail.com>
2024-01-17 22:12:49 +0100

    Rename the main class

--- src/main/java/com/pixelsapphire/wanmin/Wanmin.java
+++ src/main/java/com/pixelsapphire/wanmin/Wanmin.java
@@ -1,17 +1,9 @@
 package com.pixelsapphire.wanmin;
 
 import com.pixelsapphire.wanmin.controller.WanminDBController;
-import org.jetbrains.annotations.NotNull;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @SuppressWarnings({"SameParameterValue"})
-public class LabJDBC {
+public class Wanmin {
 
     public static void main(String[] args) {
         final var controller = new WanminDBController("sbd147412", "sbd147412");
