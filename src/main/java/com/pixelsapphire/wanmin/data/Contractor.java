70d03ba2558db85784c27a080f3a738178d72d38
meti-odyn <117643767+meti-odyn@users.noreply.github.com>
2024-01-17 23:47:23 +0100

    created Contractor

---
+++ src/main/java/com/pixelsapphire/wanmin/data/Contractor.java
@@ -0,0 +1,36 @@
+package com.pixelsapphire.wanmin.data;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Contractor {
+    private final @NotNull String name, address;
+    private String phone, email, NIP;
+
+    private Contractor(@NotNull String name, @NotNull String address, String phone, String email, String nip) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        NIP = nip;
+    }
+
+    @Contract("_ -> new")
+    public static @NotNull Contractor fromRecord(@NotNull ResultSet record) throws SQLException   {
+        return new Contractor(record.getString("nazwa"), record.getString("adres"),
+                record.getString("telefon"), record.getString("email"), record.getString("NIP"));
+    }
+
+    public @NotNull String getName() {return name;}
+
+    public @NotNull String getAddress() {return address;}
+
+    public String getPhone() {return phone;}
+
+    public String getEmail() {return email;}
+
+    public String getNIP() {return NIP;}
+}
