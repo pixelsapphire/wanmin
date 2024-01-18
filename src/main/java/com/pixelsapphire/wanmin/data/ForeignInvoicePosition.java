67434b403b4b94a0d704f588e7dee0b0d93090d4
meti-odyn <117643767+meti-odyn@users.noreply.github.com>
2024-01-18 01:09:38 +0100

    created ForeignInvoicePosition & Storage

---
+++ src/main/java/com/pixelsapphire/wanmin/data/ForeignInvoicePosition.java
@@ -0,0 +1,52 @@
+package com.pixelsapphire.wanmin.data;
+
+import com.pixelsapphire.wanmin.controller.Provider;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class ForeignInvoicePosition {
+
+    private final @NotNull ForeignInvoice invoice;
+    private final @NotNull Product product;
+    private final int price, amount;
+    private final Date date;
+
+    private ForeignInvoicePosition(@NotNull ForeignInvoice invoice, @NotNull Product product, int price, int amount, Date date) {
+        this.invoice = invoice;
+        this.product = product;
+        this.price = price;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    @Contract("_, _, _ -> new")
+    public static @NotNull ForeignInvoicePosition fromRecord(@NotNull ResultSet record, @NotNull Provider<ForeignInvoice> invoiceProvider,
+                                                             @NotNull Provider<Product> productProvider) throws SQLException {
+        return new ForeignInvoicePosition(invoiceProvider.getByValue(record.getString("faktura")), productProvider.getByValue(record.getString("produkt")),
+                record.getInt("cena"), record.getInt("ilosc"), record.getDate("data_waznosci"));
+    }
+
+    public @NotNull Product getProduct() {
+        return product;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public @NotNull ForeignInvoice getInvoice() {
+        return invoice;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ src/main/java/com/pixelsapphire/wanmin/data/Storage.java
@@ -0,0 +1,46 @@
+package com.pixelsapphire.wanmin.data;
+
+import com.pixelsapphire.wanmin.controller.Provider;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class Storage {
+    private final @NotNull Product product;
+    private final int amount;
+    private Date terminDue;
+    private final @NotNull ForeignInvoice invoice;
+
+    private Storage(@NotNull Product product, int amount, Date terminDue, @NotNull ForeignInvoice invoice) {
+        this.product = product;
+        this.amount = amount;
+        this.terminDue = terminDue;
+        this.invoice = invoice;
+    }
+
+    @Contract("_, _, _ -> new")
+    public static @NotNull Storage fromRecord(@NotNull ResultSet record, @NotNull Provider<Product> productProvider,
+                                              @NotNull Provider<ForeignInvoice> invoiceProvider) throws SQLException {
+        return new Storage(productProvider.getByValue(record.getString("produkt")), record.getInt("ilosc"),
+                record.getDate("data_waznosci"), invoiceProvider.getByValue(record.getString("faktura")));
+    }
+
+    public Date getTerminDue() {
+        return terminDue;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public @NotNull ForeignInvoice getInvoice() {
+        return invoice;
+    }
+
+    public @NotNull Product getProduct() {
+        return product;
+    }
+}
